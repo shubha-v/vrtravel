df7795586628bbd75ff5cd8cbb363ae31a623f8d
shubha-v <vshubha1998@gmail.com>
2024-08-29 17:48:21 +0530

    deleted few pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from "react"
 import './App.css'
-import { BrowserRouter as Router,Route, Routes } from 'react-router-dom';
-import Home from './Components/Home'
 
 const App = () => {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-            </Routes>
-        </Router>
+        <>
+            <p>Heloooo</p>
+        </>
     )
 }
 
